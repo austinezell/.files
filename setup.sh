dc6663acbb8ef12af576cd31e32ac6705c480da6
Austin Ezell <austin.r.ezell@gmail.com>
2016-03-11 20:01:31 -0800

    removes nvm install and instead prompts user to install

--- setup.sh
+++ setup.sh
@@ -1,38 +1,38 @@
-echo "adding config files to bashrc"
+# # echo "adding config files to bashrc"
+# #
+# # if [ ! -f $HOME/.bashrc ]; then
+# #   touch $HOME/.bashrc
+# # fi
+# #
+# # echo "adding aliases"
+# # if [ ! -f $HOME/.bash_aliases ]; then
+# #   touch $HOME/.bash_aliases && echo "source ~/.bash_aliases" >> $HOME/.bashrc
+# # fi
+# #
+# # if ! grep -q "testStringBananaCreme" $HOME/.bash_aliases; then
+# #   cat ./bash_aliases>> $HOME/.bash_aliases
+# # fi
+# #
+# # if ! grep -q "testStringBananaCreme" $HOME/.bashrc; then
+# #   cat ./.bash_config >> $HOME/.bashrc
+# # fi
+# #
+# # source $HOME/.bashrc
+# #
+# # echo "installing nvm"
+# # if [ ! -d $HOME/.nvm ]; then
+# #   wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
+# #   source $HOME/.bashrc $HOME/.bash_profile
+# # fi
+# #
+# # echo "adding git ignore rules"
+# #
+# # if [ ! -f $HOME/.gitignore_global ]; then
+# #   touch $HOME/.gitignore_global &&  git config --global core.excludesfile ~/.gitignore_global
+# # fi
+#
+# cat .ignore >> $HOME/.gitignore_global
 
-if [ ! -f $HOME/.bashrc ]; then
-  touch $HOME/.bashrc
-fi
 
-echo "adding aliases"
-if [ ! -f $HOME/.bash_aliases ]; then
-  touch $HOME/.bash_aliases && echo "source ~/.bash_aliases" >> $HOME/.bashrc
-fi
-
-
-if [! grep -q "testStringBananaCreme" $HOME/.bash_aliases]; then
-  cat ./bash_aliases>> $HOME/.bash_aliases
-fi
-
-if [! grep -q "testStringBananaCreme"] $HOME/.bashrc]; then
-  cat ./.bash_config >> $HOME/.bashrc
-fi
-
-source $HOME/.bashrc
-
-echo "installing nvm"
-if [ ! -d $HOME/.nvm ]; then
-  wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
-  source $HOME/.bashrc $HOME/.bash_profile
-fi
-
-echo "installing node"
-nvm install stable
-
-echo "adding git ignore rules"
-
-if [ ! -f $HOME/.gitignore_global ]; then
-  touch $HOME/.gitignore_global &&  git config --global core.excludesfile ~/.gitignore_global
-fi
-
-cat .ignore >> $HOME/.gitignore_global
+echo \n\n\n
+echo "install node and run nconfig"
