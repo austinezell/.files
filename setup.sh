c99d49e9c69b5ef65b000f069061a1891598800b
Austin Ezell <austin.r.ezell@gmail.com>
2019-05-13 13:52:16 -0700

    update aliases and script

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
-echo "adding config files to bashrc"
-
+echo "Adding config files to bashrc"
 if [ ! -f $HOME/.bashrc ]; then
   touch $HOME/.bashrc
 fi
 
-echo "adding aliases"
+if ! grep -q "testStringBananaCreme" $HOME/.bashrc; then
+  cat ./.bash_config >> $HOME/.bashrc
+fi
+
+echo "Adding aliases"
 if [ ! -f $HOME/.bash_aliases ]; then
   touch $HOME/.bash_aliases && echo "source ~/.bash_aliases" >> $HOME/.bashrc
 fi
@@ -13,25 +16,22 @@ if ! grep -q "testStringBananaCreme" $HOME/.bash_aliases; then
   cat ./bash_aliases>> $HOME/.bash_aliases
 fi
 
-if ! grep -q "testStringBananaCreme" $HOME/.bashrc; then
-  cat ./.bash_config >> $HOME/.bashrc
+echo 'Adding Functions'
+if [ ! -f $HOME/.bash_functions ]; then
+  touch $HOME/.bash_functions && echo "source ~/.bash_functions" >> $HOME/.bashrc
 fi
 
+if ! grep -q "testStringBananaCreme" $HOME/.bash_functions; then
+  cat ./.functions >> $HOME/.bash_functions
+fi
+
+echo 'Re-sourcing bashrc'
 source $HOME/.bashrc
 
-echo "installing nvm"
-if [ ! -d $HOME/.nvm ]; then
-  wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
-  source $HOME/.bashrc $HOME/.bash_profile
-fi
-
-echo "adding git ignore rules"
-
+echo 'Adding git ignore rules'
 if [ ! -f $HOME/.gitignore_global ]; then
   touch $HOME/.gitignore_global &&  git config --global core.excludesfile ~/.gitignore_global
 fi
-
-cat .ignore >> $HOME/.gitignore_global
-
-
-printf "\n\n\n------------install node and run nconfig----------\n\n\n\n\n"
+if ! grep -q "testStringBananaCreme" $HOME/.gitignore_global; then
+  cat .ignore >> $HOME/.gitignore_global
+fi
