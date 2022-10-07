67e952fd1251ba0d469c41e7a6023804b0fe7544
Austin Ezell <austin.r.ezell@gmail.com>
2022-10-07 13:57:30 -0700

    refactor: prefer to leave files in place

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # .files
 bash aliases and commands. includes shell script which should only be ran from fresh install
+
+
+## TODO
+Just leave these files as is and source them instead of 
--- setup.sh
+++ setup.sh
@@ -1,10 +1,28 @@
+#! /bin/bash
+FILES_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+CONFIG_LOCATION="$FILES_DIR/.bash_config"
+INCLUDE_CONFIG="include $CONFIG_LOCATION"
+
+ALIAS_LOCATION="$FILES_DIR/.aliases"
+INCLUDE_ALIAS="include $ALIAS_LOCATION"
+
+FUNCTION_LOCATION="$FILES_DIR/.functions"
+INCLUDE_FUNCTION="include $FUNCTION_LOCATION"
+
+
+# Source functions for the include function
+source $FILES_DIR/.functions
+
+
 echo "Adding config files to bashrc"
 if [ ! -f $HOME/.bashrc ]; then
   touch $HOME/.bashrc
 fi
 
-if ! grep -q "testStringBananaCreme" $HOME/.bashrc; then
-  cat ./.bash_config >> $HOME/.bashrc
+
+if ! grep -q "$INCLUDE_CONFIG" $HOME/.bashrc; then
+  echo "$INCLUDE_CONFIG" >> $HOME/.bashrc
 fi
 
 echo "Adding aliases"
@@ -12,8 +30,8 @@ if [ ! -f $HOME/.bash_aliases ]; then
   touch $HOME/.bash_aliases && echo "source ~/.bash_aliases" >> $HOME/.bashrc
 fi
 
-if ! grep -q "testStringBananaCreme" $HOME/.bash_aliases; then
-  cat ./.aliases >> $HOME/.bash_aliases
+if ! grep -q $INCLUDE_ALIAS $HOME/.bash_aliases; then
+  echo $INCLUDE_ALIAS >> $HOME/.bash_aliases
 fi
 
 echo 'Adding Functions'
@@ -21,8 +39,8 @@ if [ ! -f $HOME/.bash_functions ]; then
   touch $HOME/.bash_functions && echo "source ~/.bash_functions" >> $HOME/.bashrc
 fi
 
-if ! grep -q "testStringBananaCreme" $HOME/.bash_functions; then
-  cat ./.functions >> $HOME/.bash_functions
+if ! grep -q "$INCLUDE_FUNCTION" $HOME/.bash_functions; then
+  echo "$INCLUDE_FUNCTION" >> $HOME/.bash_functions
 fi
 
 echo 'Re-sourcing bashrc'
