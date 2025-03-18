a4cf64b9d4568cd0003033a84d2ba0f856ead03e
Austin Ezell <austin.r.ezell@gmail.com>
2025-03-17 21:51:19 -0700

    refactor: clear out uncessary include comments

--- setup.sh
+++ setup.sh
@@ -1,20 +1,6 @@
 #! /bin/bash
 FILES_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-CONFIG_LOCATION="$FILES_DIR/.bash_config"
-INCLUDE_CONFIG="include $CONFIG_LOCATION"
-
-ALIAS_LOCATION="$FILES_DIR/.aliases"
-INCLUDE_ALIAS="include $ALIAS_LOCATION"
-
-FUNCTION_LOCATION="$FILES_DIR/.functions"
-SOURCE_FUNCTIONS="source $FUNCTION_LOCATION"
-
-
-# Source functions for the include function
-source $FILES_DIR/.functions
-
-
 echo "Adding config files to bashrc"
 if [ ! -f $HOME/.bashrc ]; then
   touch $HOME/.bashrc
@@ -26,22 +12,10 @@ if [ ! -f $HOME/.bash_functions ]; then
   touch $HOME/.bash_functions && echo "source ~/.bash_functions" >> $HOME/.bashrc
 fi
 
-if ! grep -q "$SOURCE_FUNCTIONS" $HOME/.bash_functions; then
-  echo "$SOURCE_FUNCTIONS" >> $HOME/.bash_functions
-fi
-
-
-if ! grep -q "$INCLUDE_CONFIG" $HOME/.bashrc; then
-  echo "$INCLUDE_CONFIG" >> $HOME/.bashrc
-fi
-
-
-
 echo "Adding aliases"
 if [ ! -f $HOME/.bash_aliases ]; then
   touch $HOME/.bash_aliases && echo "source ~/.bash_aliases" >> $HOME/.bashrc
 fi
-
 
 echo 'Re-sourcing bashrc'
 source $HOME/.bashrc
